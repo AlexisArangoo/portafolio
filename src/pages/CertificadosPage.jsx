4ebf35d74ce93eac6c80bd90b1249e8354660a82
Alexis Arango <134032888+AlexisArangoo@users.noreply.github.com>
2024-12-19 21:06:48 -0500

    Integracion de motion para las animaciones

--- src/pages/CertificadosPage.jsx
+++ src/pages/CertificadosPage.jsx
@@ -9,40 +9,40 @@ const CertificadosPage = () => {
   return (
     <article className={`certificados ${onDark ? 'ondark' : ''}`}>
       <motion.section className="certificados-section ci"
-        initial={{opacity: 0.5, x:-600}}
+        initial={{opacity: 0.5, x:'-90%'}}
         whileInView={{opacity: 1, x:0}}
         transition={{ duration: 0.5 }}
-        viewport={{amount: 0.5 }}
+        viewport={{amount: 0.3 }}
       >
         <h2 className='notranslate certificados_title'>FULL STACK</h2>
         <img src="/certificados/fullstack.png" alt="" />
       </motion.section>
 
       <motion.section className="certificados-section cd"
-        initial={{opacity: 0.5, x:-600}}
+        initial={{opacity: 0.5, x:'-90%'}}
         whileInView={{opacity: 1, x:0}}
         transition={{ duration: 0.5 }}
-        viewport={{amount: 0.5 }}
+        viewport={{amount: 0.3 }}
       >
         <h2 className='notranslate certificados_title'>FRONT-END</h2>
         <img src="/certificados/react.png" alt="" />
       </motion.section>
      
       <motion.section className="certificados-section ci"
-        initial={{opacity: 0.5, x:-600}}
+        initial={{opacity: 0.5, x:'-90%'}}
         whileInView={{opacity: 1, x:0}}
         transition={{ duration: 0.5 }}
-        viewport={{amount: 0.5 }}
+        viewport={{amount: 0.3 }}
       >
         <h2 className='notranslate certificados_title'>BACK-END</h2>
         <img src="/certificados/backend.png" alt="" />
       </motion.section> 
 
       <motion.section className="certificados-section cd"
-        initial={{opacity: 0.5, x:-600}}
+        initial={{opacity: 0.5, x:'-90%'}}
         whileInView={{opacity: 1, x:0}}
         transition={{ duration: 0.5 }}
-        viewport={{amount: 0.5 }}
+        viewport={{amount: 0.3 }}
       >
         <h2 className=' certificados_title'>FUNDAMENTOS</h2>
         <img src="/certificados/fundamentos.jpg" alt="" />
